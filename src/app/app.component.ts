140335fe9f427244094739fc6a4dfad6595055c3
Ignatius Baskar <PREMKUMARAN.BASKAR@Chubb.com>
2020-07-03 12:39:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveForms';
+  userForm;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm= this.fb.group({
+      'name': fb.control('', [Validators.required, Validators.maxLength(20)] ),
+      'age': fb.control('', Validators.required),
+      'country': fb.control('IN'),
+      'email': fb.control(''),
+      'mobile': fb.control('')
+    })
+  }
+
+  ngOnInit() {
+  
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Name', form.value.name);
+    console.log('Email', form.value.email);
+    console.log('Message', form.value.country);
+  }
 }
